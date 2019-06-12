529f6b5d121f6741d0b1d84cc190bedd1e574776
Sachinthani Jayarathna <sachinthani.j@casterlycrown.com>
2019-06-12 14:37:52 +0530

    Adding sample node

--- config/main.js
+++ config/main.js
@@ -2,5 +2,5 @@ module.exports = {
     // Database connection information
     'database': 'mongodb://[add your database here]',
     // Setting port for server
-    'port': process.env.PORT || 5000,
+    'port': process.env.PORT || 3000,
 };
